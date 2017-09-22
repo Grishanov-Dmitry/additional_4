3874050d327136984ff2f81ffeb473fab2204e3b
Grishanov_Dmitry <grishanov.ds@gmail.com>
2017-09-22 16:15:17 +0300

    Test finished 100%

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function multiply(first, second) {
-  // your solution
+
+    a = first.split('').reverse();
+    b = second.split('').reverse();
+    let result = [];
+
+    for (let i = 0; a[i] >= 0; i++) {
+        for (let j = 0; b[j] >= 0; j++) {
+            if (!result[i + j]) {
+                result[i + j] = 0;
+            }
+
+            result[i + j] += a[i] * b[j];
+        }
+    }
+
+    for (let i = 0; result[i] >= 0; i++) {
+        if (result[i] >= 10) {
+            if (!result[i + 1]) {
+                result[i + 1] = 0;
+            }
+
+            result[i + 1] += parseInt(result[i] / 10);
+            result[i] %= 10;
+        }
+    }
+
+    return result.reverse().join('');
 }
